80b54dca708c352c1d1c8fef09b3efa481d10333
Vira <anafkn@gmail.com>
2023-07-23 23:29:56 +0200

    Added relevant icon from the API

--- src/index.js
+++ src/index.js
@@ -41,6 +41,13 @@ function displayWeatherCondition(response) {
 
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
 
 }
 
